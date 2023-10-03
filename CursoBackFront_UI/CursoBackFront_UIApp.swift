216938bc30acabe08d52c731eac81b2e98c7a592
Horacio Mota <horaciomota93@gmail.com>
2023-10-03 16:47:12 +0100

    first commit

---
+++ CursoBackFront_UI/CursoBackFront_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CursoBackFront_UIApp.swift
+//  CursoBackFront_UI
+//
+//  Created by Horacio Mota on 03/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct CursoBackFront_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
